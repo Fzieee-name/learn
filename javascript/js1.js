56baa45d79d3085a503f65524142069eae7682ce
Fziee <fauziabhinayazaidan@smkitihsanulfikri.sch.id>
2026-03-18 20:41:50 +0700

    js1.js

---
+++ javascript/js1.js
@@ -0,0 +1,24 @@
+let nama = 'saya' //string
+let usia = 30 //integer
+let tinggiBadan = 173.4 //double atau float
+let beratBadan //undefined
+let mieAyam = null // null atau ga da istilahya
+
+beratBadan = 60
+
+if(mieAyam == null){
+    mieAyam = 'aku lapar'
+} else {
+    mieAyam = 'aku wareg'
+}
+
+let duit = 5000
+let tambahan = 2000
+const utang = 1500
+const total = duit + tambahan - utang
+
+alert(`nama ${nama} usia ${usia} tinggi badan ${tinggiBadan}cm berat badan ${beratBadan}kg mie ayam ${mieAyam}`
+
+)
+
+alert(`duit guwa Rp.${duit} ditambahin sama temen guwa Rp.${tambahan} totalnya Rp.${total}`)
