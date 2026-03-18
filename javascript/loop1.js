596291ce0b7b56a018859875decf2376d389ede1
Fziee <fauziabhinayazaidan@smkitihsanulfikri.sch.id>
2026-03-18 21:29:24 +0700

    Create loop1.js

---
+++ javascript/loop1.js
@@ -0,0 +1,19 @@
+//for loop
+
+const nama = ['asep', 'saya', 'pepe', 'yolo']
+
+for(let i = 0; i < nama.length; i++){
+    console.log(nama[i])
+} 
+
+// while loop
+i=0
+while(i<10){ // while cek dulu baru eksekusi
+    i++
+    console.log(i)
+}
+
+do { //do while eksekusi baru di cek tetap akan dieksekusi sekali walau perintah syntax tidak sesuai
+    i++
+    console.log(i)
+} while (i < 10)
